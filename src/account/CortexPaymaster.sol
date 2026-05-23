ab251ef6d78d6c49eda521be4bbfc13361ea1d6b
maxmoka <ma1x1moka@gmail.com>
2026-05-24 01:50:32 +0200

    feat: add CortexPaymaster with Autoria participant validation

--- src/account/CortexPaymaster.sol
+++ src/account/CortexPaymaster.sol
@@ -4,28 +4,35 @@ pragma solidity ^0.8.27;
 
 import "./CortexAccount.sol";
 import "../core/Autoria.sol";
+import {IEntryPoint} from "account-abstraction/interfaces/IEntryPoint.sol";
 
 contract CortexPaymaster {
-    constructor(address _cortexAccount, address _autoria) {
+    constructor(address _cortexAccount, address _autoria, address _entryPoint) {
         cortexAccount = CortexAccount(_cortexAccount);
         autoria = Autoria(_autoria);
+        entryPoint = IEntryPoint(_entryPoint);
     }
-    // errors
+    // variabless
 
-    // error InvalidAddr();
+    IEntryPoint public immutable entryPoint;
 
     CortexAccount public cortexAccount;
     Autoria public autoria;
 
     // functions
-    function validatePaymasterUserOp(CortexAccount.UserOperation calldata user0p, bytes32 user0pHash, uint256 maxCost)
-        external
-        returns (bytes memory context, uint256 validationData)
-    {
+    function validatePaymasterUserOp(
+        CortexAccount.UserOperation calldata user0p,
+        bytes32 user0pHash,
+        uint256 maxCost
+    ) external returns (bytes memory context, uint256 validationData) {
         if (autoria.getDealAddress(user0p.sender).length == 0) {
             return (bytes(""), 1);
         } else {
             return (bytes(""), 0);
         }
+    }
+
+    receive() external payable {
+        entryPoint.depositTo{value: msg.value}(address(this));
     }
 }
